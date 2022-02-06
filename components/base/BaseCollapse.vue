f76974e3f686617da2521c57e661f276fd5dbe58
Leigh Dinaya <leighdinaya@protonmail.com>
2022-02-06 08:54:51 +0800

    feat: add collapsing component

---
+++ components/base/BaseCollapse.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="flex flex-col">
+		<div>
+			<button
+				class="flex items-center text-sm border rounded-lg px-2 py-1"
+				@click="toggle = !toggle"
+			>
+				Filter
+				<span
+					><svg
+						v-if="!toggle"
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-4 w-4"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M5 15l7-7 7 7"
+						/>
+					</svg>
+					<svg
+						v-else
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-4 w-4"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M19 9l-7 7-7-7"
+						/>
+					</svg>
+				</span>
+			</button>
+		</div>
+
+		<div v-show="toggle" class="bg-slate-800 p-2 mt-2 rounded-lg">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			toggle: false,
+		}
+	},
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,35 +2,46 @@
 	<div>
 		<app-hero />
 		<hr class="mt-5 mb-5" />
-		<div class="btn-group">
-			<button
-				class="dark:bg-slate-800 bg-slate-200 px-2 py-1 text-sm rounded-full"
-				@click="sortBy('desc')"
-			>
-				Latest
-			</button>
-			<button
-				class="dark:bg-slate-800 bg-slate-200 px-2 py-1 text-sm rounded-full"
-				@click="sortBy('asc')"
-			>
-				Oldest
-			</button>
-		</div>
-		<div class="md:flex flex-col-reverse md:flex-row items-baseline">
+		<div class="">
+			<base-collapse class="mt-5">
+				<div class="btn-group">
+					<button
+						class="
+							border
+							dark:bg-slate-800
+							bg-slate-200
+							px-2
+							py-1
+							text-sm
+							rounded-full
+						"
+						@click="sortBy('desc')"
+					>
+						Latest
+					</button>
+					<button
+						class="
+							border
+							px-2
+							py-1
+							text-sm
+							import
+							CollapseComp
+							from
+							'~/components/base/CollapseComp.vue'
+							rounded-full
+						"
+						@click="sortBy('asc')"
+					>
+						Oldest
+					</button>
+				</div>
+				<app-tags
+					:tags="tags"
+					class="dark:bg-slate-800 bg-slate-200 p-2 md:mt-0 mt-5 rounded-lg"
+				/>
+			</base-collapse>
 			<notes-list :notes="notes" class="mt-2 flex-1" />
-			<app-tags
-				:tags="tags"
-				class="
-					md:w-1/4
-					w-full
-					dark:bg-slate-800
-					bg-slate-200
-					rounded-md
-					p-2
-					md:mt-0
-					mt-2
-				"
-			/>
 		</div>
 	</div>
 </template>
